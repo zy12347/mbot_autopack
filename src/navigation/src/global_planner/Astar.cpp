536fc49bbe5cdea98db3defd9e179b8f7b07b57b
zy <zhangy@ldrobot.com>
2025-09-04 16:30:40 +0800

    更新最新的Astar算法实现

--- src/navigation/src/global_planner/Astar.cpp
+++ src/navigation/src/global_planner/Astar.cpp
@@ -3,48 +3,79 @@ std::vector<std::pair<int, int>> Astar::PlanPath(int start_idx, int start_idy,
                                                  int target_idx,
                                                  int target_idy) {
   if (map_data_ == nullptr) {
-    return;
+    return std::vector<std::pair<int, int>>();
   }
-  GridNode base_node = GridNode(start_idx, start_idy, 0);
-  next_list_.emplace(base_node);
-  while (!next_list_.empty()) {
-    GridNode cur_node = next_list_.top().node;
-    next_list.pop();
-    if (cur_node.x == targer_idx && cur_node.y == target_idy) {
-      break;
+  target_idx_ = target_idx;
+  target_idy_ = target_idy;
+  open_map_[start_idy * width_ + start_idx] = std::make_pair(0, -1);
+  while (!open_map_.empty()) {
+    int cur_node_index = GetMinScoreIndex();
+    if (IsTarget(cur_node_index % width_, cur_node_index / width_)) {
+      return BuildPath(cur_node_index % width_, cur_node_index / width_);
     }
-    ProcessNeighbor(cur_node);
+    ProcessNeighbor(cur_node_index % width_, cur_node_index / width_);
   }
-  BuildPath();
+  return std::vector<std::pair<int, int>>();
 }
 
-void Astar::ProcessNeighbor(GridNode& node) {
+int Astar::GetMinScoreIndex() {
+  float min_score = std::numeric_limits<float>::max();
+  int min_node_index = -1;
+  for (auto it = open_map_.begin(); it != open_map_.end(); it++) {
+    if (it->second.first < min_score) {
+      min_score = it->second.first;
+      min_node_index = it->first;
+    }
+  }
+  open_map_.erase(min_node_index);
+  return min_node_index;
+}
+void Astar::ProcessNeighbor(int x,int y) {
+  close_map_[y * width_ + x] = open_map_[y * width_ + x].second;
   for (int dx = -1; dx <= 1; dx++) {
     for (int dy = -1; dy <= 1; dy++) {
-      if (dx == 0 && dy == 0)
+      if (!IsValid(x + dx, y + dy))
         continue;
-      ProcessNode(node.x + dx, node.y + dy);
+      float base_score = open_map_[y * width_ + x].first + 1;
+      float heuristic_score = HeuristicScore(x + dx, y + dy);
+      float total_score = base_score + heuristic_score;
+      int index = (y + dy) * width_ + (x + dx);
+      if(open_map_.find(index) == open_map_.end()){
+        open_map_[index] = std::make_pair(total_score, y * width_ + x);
+      }else if(open_map_[index].first > total_score){
+        open_map_[index] = std::make_pair(total_score, y * width_ + x);
+      }
     }
   }
 }
-
-void Astar::ProcessNode(int x, int y) {
-  if (x >= 0 && x < width_ && y >= 0 && y < height_ && map_data_[index] > 0) {
-    int index = y * width_ + x;
-    if (open_close_set_[index] == 0) {
-      float total_score = Totalscore(x, y);
-      GridWithScore cur_node_score(x, y, total_score);
-      next_list_.push(cur_node_score);
-      open_close_set_[index] = 1;
-    }
+std::vector<std::pair<int, int>> BuildPath(int x,int y) {
+  std::vector<std::pair<int, int>> path;
+  int current_index = y * width_ + x;
+  while(current_index != -1){
+    path.emplace_back(current_index % width_, current_index / width_);
+    current_index = close_map_[current_index];
   }
+  return std::reverse(path.begin(), path.end());
 }
 
-float Astar::BaseScore(int x, int y) {}
-float Astar::HeuristicScore(int x, int y) {}
-float Astar::TotalScore(int x, int y) {
-  float base_score = BaseScore(x, y);
-  float heuristic_score = HeuristicScore(x, y);
-  float total_score = heuristic_score + base_score;
-  return total_score;
-}
+// void Astar::ProcessNode(GridNode& neighbor_node) {
+//   float total_score = TotalScore(neighbor_node);
+//   if(open_map_.find(index) == open_map_.end()){
+//     open_map_[index] = total_score;
+//   }else if(open_map_[index] > total_score){
+//     open_map_[index] = total_score;
+//   }
+// }
+
+// float Astar::BaseScore(GridNode& node) {
+//   return node.parent->score + 1;
+// }
+// float Astar::HeuristicScore(int x, int y) {
+//   return std::abs(x - target_idx_) + std::abs(y - target_idy_);
+// }
+// float Astar::TotalScore(GridNode& node) {
+//   float base_score = BaseScore(node);
+//   float heuristic_score = HeuristicScore(node);
+//   float total_score = heuristic_score + base_score;
+//   return total_score;
+// }
--- src/navigation/src/global_planner/Astar.h
+++ src/navigation/src/global_planner/Astar.h
@@ -1,18 +1,27 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <priority_queue>
+// #include <priority_queue>
 #include <vector>
+#include <unordered_map>
 
-#include "GridNode.h"
-struct GridWithScore {
-  GridWithScore(int x, int y, float score) : node(x, y), score(score);
-  GridNode node;
-  int score;
-  bool operator(const GridWithScore& other) {
-    return score > other.score;
-  }
-};
+// struct GridNode{
+//   GridNode(int x, int y) : x(x), y(y);
+//   int x;
+
+//   int y;
+//   int base_score = 0;
+//   int heuristic_score = 0;
+//   int total_score = 0;
+
+//   GridNode* parent = nullptr;
+//   bool operator>(const GridNode& other) {
+//     return total_score > other.total_score;
+//   }
+//   bool operator==(const GridNode& other) const {
+//         return x == other.x && y == other.y;
+//     }
+// };
 
 class Astar {
  public:
@@ -22,24 +31,31 @@ class Astar {
   };
   ~Astar();
   void UpdateMap(uint8_t* map_data);
-  void ProcessNode(int x, int y);
-  void ProcessNeighbor(GridNode& node);
+  void ProcessNeighbor(int x,int y);
   std::vector<std::pair<int, int>> PlanPath(int start_idx, int start_idy,
                                             int target_idx, int target_idy);
 
  private:
-  float BaseScore(int x, int y);
-  float HeuristicScore(int x, int y);
-  float TotalScore(int x, int y);
+  // float BaseScore(GridNode& node);
+  // float HeuristicScore(GridNode& node);
+  // float TotalScore(GridNode& node);
 
-  bool IsTarget(GridNode* node);
-  std::vector<std::pair<int, int>> BuildPath();
+  bool IsValid(int x,int y){
+    return x >= 0 && x < width_ && y >= 0 && y < height_ && map_data_[y * width_ + x] > 0 && close_map_.find(y * width_ + x) == close_map_.end();
+  };
+  bool IsTarget(int x,int y){
+    return x == target_idx && y == target_idy;
+  };
+  int GetMinScoreIndex();
+  std::vector<std::pair<int, int>> BuildPath(int x,int y);
   uint8_t* map_data_ = nullptr;
   int width_ = 100;
   int height_ = 100;
   int size_ = width_ * height_;
 
-  std::priority_queue<GridWithScore> next_list_;
-  GridNode* root;
-  std::bitset<size_> open_close_set_;
-}
+  int target_idx_ = 0;
+  int target_idy_ = 0;
+
+  std::unordered_map<int,std::pair<float,int>> open_map_;//表示在open_list_中的节点的index及其total_score
+  std::unordered_map<int,int> close_map_;//表示在close_list_中的节点的index及其pre_index
+};
